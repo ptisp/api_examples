378fcf8a748b20880e45212bbd1416b9b095b470
Pedro Dias <petermdias@gmail.com>
2012-03-14 15:43:47 +0000

    api v1

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require "RestRequest.inc.php";
 
-$request = new RestRequest('https://www.ptisp.pt/api/cloud/pg0000/1/status', 'GET');
+$request = new RestRequest('https://www.ptisp.pt/api/cloud/1/status', 'GET');
 $request->setUsername("pt0000");
 $request->setPassword("asdafagasgsagasgasgdasgdasgasgdsgdag");
 $request->execute();
