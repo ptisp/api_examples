efa1efe4cda442403f5441ac9a4e9ad2169a64b0
Pedro Dias <petermdias@gmail.com>
2012-05-07 18:14:33 +0200

    Updated, to allow post method.

--- Android/APIRequest.java
+++ Android/APIRequest.java
@@ -3,6 +3,8 @@ package com.ptisp;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
@@ -25,18 +27,29 @@ public class APIRequest {
 	}
 
 	public JSONObject execute() {
+		return execute(false);
+	}
+
+	public JSONObject execute(boolean isPost) {
 		try {
 			HttpClient client = new DefaultHttpClient();
 
 			String base64EncodedCredentials = Base64.encodeBytes((username
 					+ ":" + password).getBytes());
 
-			HttpGet post = new HttpGet(url);
+			HttpRequestBase req;
+			if (isPost) {
+				req = new HttpPost(url);
+			} else {
+				req = new HttpGet(url);
+			}
 
-			post.addHeader("Authorization", "Basic " + base64EncodedCredentials);
+			req.addHeader("Authorization", "Basic " + base64EncodedCredentials);
 
 			ResponseHandler<String> responseHandler = new BasicResponseHandler();
-			String responseBody = client.execute(post, responseHandler);
+			String responseBody = client.execute(req, responseHandler);
+
+			Log.d("PTISP", "BODY: " + responseBody);
 
 			try {
 				return new JSONObject(responseBody);
